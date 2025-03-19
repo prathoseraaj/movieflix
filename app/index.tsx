fa2bf5b8ed0f15a82fff624aea8e7535d09c5e0d
prathoseraaj V <146630697+prathoseraaj@users.noreply.github.com>
2025-03-19 19:23:24 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>HELLLOOOOOOOOOOOOOOooooo harshan gayyyyyyyyyyyyyy</Text>
+      <Text>HELLLOOOOOOOOOOO</Text>
     </View>
   );
 }
