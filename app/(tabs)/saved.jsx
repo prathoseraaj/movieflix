b95caafd517735b0e7cc0a6e271dae373de59fea
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-20 16:14:14 +0530

    fix: saved

--- app/(tabs)/saved.jsx
+++ app/(tabs)/saved.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const saved = () => {
   return (
     <View className= "flex-1 justify-center items-center">
-      <Text className="text-5xl text-accent font-bold">Saved</Text>
+      <Text className="text-5xl text-accent font-bold">SAVED</Text>
     </View>
   )
 }
