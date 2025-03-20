0d077b237b4d7cdb5d04053cbee73390ec2c632c
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-20 14:49:23 +0530

    feat: routing

---
+++ app/onboard.jsx
@@ -0,0 +1,16 @@
+//rnfce
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const onboard = () => {
+  return (
+    <View>
+      <Text>onboard</Text>
+    </View>
+  )
+}
+
+export default onboard
+
+const styles = StyleSheet.create({})
