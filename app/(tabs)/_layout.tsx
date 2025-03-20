d5a937b42ac08afb87c7e1ea716b06f8b384776e
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-20 15:39:10 +0530

    feat: nativewind

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 const _layout = () => {
+
+  
+
   return (
     <Tabs>
       <Tabs.Screen
@@ -10,6 +15,16 @@ const _layout = () => {
         options={{
           title: "home",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[100px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+
+                <Image source= {icons.home} tintColor="#151312" className="size-5"/>
+                <Text className="text-secondary text-base font-semibold">Home</Text>
+
+              </ImageBackground>
+            </>
+          ),
         }}
       />
 
@@ -29,7 +44,7 @@ const _layout = () => {
         }}
       />
 
-<Tabs.Screen
+      <Tabs.Screen
         name="profile"
         options={{
           title: "profile",
