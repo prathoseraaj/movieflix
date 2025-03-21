418d9409e6b25adaf25d37d8a3c772398301aee4
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-21 15:20:40 +0530

    feat: scroll view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,9 @@ export default function Index() {
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
-      <ScrollView className="flex-1 px-5">
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{
+        minHeight: "100%", paddingBottom: 10
+      }}>
           <Image source={icons.logo} className="w-12 h-10  mt-20 mb-5 mx-auto"/>
       </ScrollView>
 
