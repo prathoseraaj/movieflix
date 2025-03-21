aab4451a335712d454f04c59e7bb59faa7f8c226
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-21 12:07:54 +0530

    feat: image and logo

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Link } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+      <ScrollView className="flex-1 px-5">
+          <Image source={icons.logo} className="w-12 h-10  mt-20 mb-5 mx-auto"/>
+      </ScrollView>
+
+    </View>
+  );
+}
