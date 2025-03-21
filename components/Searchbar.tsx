933b1c92b7ddaea20ed74497177b7b70391044a5
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-21 15:40:15 +0530

    feat: search bar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
-const Searchbar = () => {
-  return (
-    <View>
-      <Text className='text-white'>Searchbar</Text>
-    </View>
-  )
+interface props{
+  placeholder: string;
+  onPress?: () => void;
 }
 
-export default Searchbar
+const Searchbar = ({placeholder, onPress}: props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4 ">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChange={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default Searchbar;
