b28db93fa73716d7e9374971c26c611e6ffab8e4
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-22 18:08:32 +0530

    feat: rating and date

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { icons } from "@/constants/icons";
 
 const MovieCard = ({
   id,
@@ -11,7 +12,7 @@ const MovieCard = ({
 }: Movie) => {
   return (
     //@ts-ignore
-   <Link href={`/movies/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -19,10 +20,24 @@ const MovieCard = ({
               ? `https://image.tmdb.org/t/p/w500${poster_path}`
               : `https://placehold.co/600x400/1a1a1a/ffffff.png`,
           }}
-          className="w-full h-52 rounded-lg " 
+          className="w-full h-52 rounded-lg "
           resizeMode="cover"
         />
         <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+        <View className="flex-row item-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4 " />
+          <Text className="text-xs text-white font-bold uppercase">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1" >
+            {release_date?.split('-')[0]}
+          </Text>
+{/*           <Text className="text-xs font-medium uppercasse text-light-300">
+            Movie
+          </Text> */}
+        </View>
       </TouchableOpacity>
     </Link>
   );
