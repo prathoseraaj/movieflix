8dc71931f75ae402754f5e53fc5186dd47059da0
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-22 14:21:49 +0530

    fix: nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -55,6 +55,17 @@ export default function Index() {
                     {item.title}
                   </Text>
                 )}
+                keyExtractor={(item)=>item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap:20,
+                  paddingRight:5,
+                  marginBottom:10,
+                }}
+
+                className="mt-2 pb-32"
+                scrollEnabled={false}
               />
 
             </>
