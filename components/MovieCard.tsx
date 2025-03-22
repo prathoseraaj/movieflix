788976170a18b36812dc54d984e465fe68ea5bc1
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-22 18:12:53 +0530

    fix: movie name to one line

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -23,7 +23,9 @@ const MovieCard = ({
           className="w-full h-52 rounded-lg "
           resizeMode="cover"
         />
-        <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {title}
+        </Text>
         <View className="flex-row item-center justify-start gap-x-1">
           <Image source={icons.star} className="size-4 " />
           <Text className="text-xs text-white font-bold uppercase">
@@ -31,10 +33,10 @@ const MovieCard = ({
           </Text>
         </View>
         <View className="flex-row items-center justify-between">
-          <Text className="text-xs text-light-300 font-medium mt-1" >
-            {release_date?.split('-')[0]}
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
           </Text>
-{/*           <Text className="text-xs font-medium uppercasse text-light-300">
+          {/*           <Text className="text-xs font-medium uppercasse text-light-300">
             Movie
           </Text> */}
         </View>
