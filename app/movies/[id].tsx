9ff9ea8760c8abc198dd582d4cc6d67cf4295a17
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-24 14:05:51 +0530

    feat: iamge added sucessfully

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import useFetch from '@/services/usefetch';
+import { fetchMoiveDetails } from '@/services/api';
 
 const MoiveDetails = () => {
+
+  const {id} = useLocalSearchParams() ;
+
+  const { data: movie, loading } = useFetch(() => fetchMoiveDetails(id as string));
+ 
   return (
-    <View>
-      <Text>MoiveDetails</Text>
+    <View className='bg-primary flex-1'>
+      <ScrollView contentContainerStyle={{
+        paddingBottom: 80
+      }}>
+        <View>
+          <Image source={{uri:`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}} className='w-full h-[550px]'/>
+        </View>
+
+      </ScrollView>
     </View>
   )
 }
